2d35547af74865dc90bcea3a37423d05d1ebbdb5
Alberto-Del <albertodelgado2003@gmail.com>
2025-05-08 09:34:40 +0200

    mejoras en las guias falta comprobacion de entrada datos, diferencia entre noticia y guia y editar guia

--- src/guides/router.js
+++ src/guides/router.js
@@ -11,7 +11,10 @@ import {
 
 const guidesRouter = express.Router();
 
-guidesRouter.post('/addGuide/:game_id', doAddGuide); //TODO: hacer comprobacion de inputs
+guidesRouter.post('/addGuide/:game_id',
+    body('title', 'No puede ser vacio').trim().notEmpty(),
+    body('content', 'No puede ser vacio').trim().notEmpty(),
+    asyncHandler(doAddGuide));
 guidesRouter.get('/addGuide/:game_id', viewAddGuide);
 guidesRouter.get('/games/:game_id/guides', listGuidesByGameId);
 guidesRouter.get('/:guide_id', showFullGuide);
